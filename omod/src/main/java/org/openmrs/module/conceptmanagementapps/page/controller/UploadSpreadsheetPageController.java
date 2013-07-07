58fb94a067c4d5f62ee05d11ab6f632fe6bec266
Jennifer Parise <jennparise@ubuntu.(none)>
2013-07-07 07:00:28 -0700

    fixed webModuleApplicationContext and fixed upload file string formatting

--- omod/src/main/java/org/openmrs/module/conceptmanagementapps/page/controller/UploadSpreadsheetPageController.java
+++ omod/src/main/java/org/openmrs/module/conceptmanagementapps/page/controller/UploadSpreadsheetPageController.java
@@ -28,33 +28,40 @@ import org.openmrs.ui.framework.page.FileDownload;
 import org.openmrs.ui.framework.page.PageModel;
 
 public class UploadSpreadsheetPageController {
-	
+
 	protected final Log log = LogFactory.getLog(this.getClass());
-	
-	public FileDownload post(@RequestParam("spreadsheet") MultipartFile spreadsheetFile, UiUtils ui) {
-		
+
+	public FileDownload post(
+			@RequestParam("spreadsheet") MultipartFile spreadsheetFile,
+			UiUtils ui) {
+
 		BufferedReader br = null;
 		String line = "";
 		ArrayList<String> linesWithErrors = new ArrayList<String>();
 		String delimiter = ",";
-		linesWithErrors.add("error reason" + delimiter + "map type" + delimiter + "source name" + delimiter + "source code"
-		        + delimiter + "concept Id" + delimiter + "concept uuid" + delimiter + "preferred name" + delimiter
-		        + "description" + delimiter + "class" + delimiter + "datatype" + delimiter + "all existing mappings" + "\n");
-		
+		linesWithErrors.add("error reason" + delimiter + "map type" + delimiter
+				+ "source name" + delimiter + "source code" + delimiter
+				+ "concept Id" + delimiter + "concept uuid" + delimiter
+				+ "preferred name" + delimiter + "description" + delimiter
+				+ "class" + delimiter + "datatype" + delimiter
+				+ "all existing mappings" + "\n");
+
 		String filename = spreadsheetFile.getOriginalFilename();
 		String[] nameWithoutExtension = filename.split(".csv");
-		String sourceId = nameWithoutExtension[0].substring(nameWithoutExtension[0].length() - 1);
-		
+		String sourceId = nameWithoutExtension[0]
+				.substring(nameWithoutExtension[0].length() - 1);
+
 		try {
-			
-			br = new BufferedReader(new InputStreamReader(spreadsheetFile.getInputStream()));
+
+			br = new BufferedReader(new InputStreamReader(
+					spreadsheetFile.getInputStream()));
 			int columnNumber = 1;
 			boolean header = true;
 			ConceptService cs = Context.getConceptService();
 			Collection<ConceptMap> conceptMappings;
 			ConceptSource source;
 			boolean foundMapping = false;
-			
+
 			while ((line = br.readLine()) != null) {
 				String errorString = "";
 				String mappings = "";
@@ -62,7 +69,7 @@ public class UploadSpreadsheetPageController {
 				String sourceCode = "";
 				String sourceName = "";
 				ConceptMap conceptMap = new ConceptMap();
-				
+
 				if (!header) {
 					if (!foundMapping && !line.endsWith("\"")) {
 						line = line + "\"";
@@ -74,118 +81,134 @@ public class UploadSpreadsheetPageController {
 						line = "\"" + line;
 						foundMapping = false;
 					}
-					
-					String[] tokens = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+
+					String[] tokens = line
+							.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
 					for (String t : tokens) {
 						switch (columnNumber) {
-							case 1:
-								mapTypeName = t.replace("\"", "").trim();
-								if (t.replace("\"", "").trim().length() <= 1
-								        || cs.getConceptMapTypeByName(mapTypeName) == null) {
-									errorString = " " + errorString + ui.message("conceptmanagementapps.file.maptype.error")
-									        + " ";
-								} else {
-									try {
-										conceptMap.setConceptMapType(cs.getConceptMapTypeByName(mapTypeName));
-									}
-									catch (APIException e) {
-										errorString = "\"" + e + "\"" + "," + line + "\n";
-										e.printStackTrace();
-									}
+						case 1:
+							mapTypeName = t.replace("\"", "").trim();
+							if (t.replace("\"", "").trim().length() <= 1
+									|| cs.getConceptMapTypeByName(mapTypeName) == null) {
+								errorString = " "
+										+ errorString
+										+ ui.message("conceptmanagementapps.file.maptype.error")
+										+ " ";
+							} else {
+								try {
+									conceptMap
+											.setConceptMapType(cs
+													.getConceptMapTypeByName(mapTypeName));
+								} catch (APIException e) {
+									errorString = " " + errorString + "\"" + e
+											+ "\"";
+									e.printStackTrace();
 								}
-								columnNumber++;
-								break;
-							case 2:
-								sourceName = t.replace("\"", "").trim();
-								if (t.replace("\"", "").trim().length() <= 1) {
-									errorString = " " + errorString
-									        + ui.message("conceptmanagementapps.file.sourcename.error") + " ";
-									
+							}
+							columnNumber++;
+							break;
+						case 2:
+							sourceName = t.replace("\"", "").trim();
+							if (t.replace("\"", "").trim().length() <= 1) {
+								errorString = " "
+										+ errorString
+										+ ui.message("conceptmanagementapps.file.sourcename.error")
+										+ " ";
+
+							}
+							columnNumber++;
+							break;
+						case 3:
+							sourceCode = t.replace("\"", "").trim();
+							source = cs.getConceptSource(Integer
+									.valueOf(sourceId.trim()));
+							if (t.replace("\"", "").trim().length() <= 1
+									|| cs.getConceptReferenceTermByCode(t,
+											source) == null) {
+								errorString = " "
+										+ errorString
+										+ ui.message("conceptmanagementapps.file.sourcecode.error")
+										+ " ";
+							} else {
+								try {
+									ConceptReferenceTerm refTerm = cs
+											.getConceptReferenceTermByCode(t,
+													source);
+									conceptMap.setConceptReferenceTerm(refTerm);
+								} catch (APIException e) {
+									errorString = " " + errorString + "\"" + e
+											+ "\"";
+									e.printStackTrace();
 								}
-								columnNumber++;
-								break;
-							case 3:
-								sourceCode = t.replace("\"", "").trim();
-								source = cs.getConceptSource(Integer.valueOf(sourceId.trim()));
-								if (t.replace("\"", "").trim().length() <= 1
-								        || cs.getConceptReferenceTermByCode(t, source) == null) {
-									errorString = " " + errorString
-									        + ui.message("conceptmanagementapps.file.sourcecode.error") + " ";
-								} else {
-									try {
-										ConceptReferenceTerm refTerm = cs.getConceptReferenceTermByCode(t, source);
-										conceptMap.setConceptReferenceTerm(refTerm);
-									}
-									catch (APIException e) {
-										errorString = "\"" + e + "\"" + "," + line + "\n";
-										e.printStackTrace();
-									}
-									
-								}
-								columnNumber++;
-								break;
-							case 4:
-								if (errorString.length() < 1) {
-									
-									if (cs.getConcept(t) != null) {
-										Concept concept = cs.getConcept(t);
-										if (concept.getConceptMappings() == null) {
-											List<ConceptMap> conceptMappingsList = new ArrayList<ConceptMap>();
-											conceptMappingsList.add(conceptMap);
-											concept.setConceptMappings(conceptMappingsList);
-										} else {
-											conceptMappings = concept.getConceptMappings();
-											conceptMappings.add(conceptMap);
-											concept.setConceptMappings(conceptMappings);
-											
-										}
-										try {
-											cs.saveConcept(concept);
-										}
-										catch (APIException e) {
-											errorString = "\"" + e + "\"" + "," + line + "\n";
-											e.printStackTrace();
-										}
-										
-									}
-								}
-								columnNumber++;
-								break;
-							case 5:
-								columnNumber++;
-								break;
-							case 6:
-								columnNumber++;
-								break;
-							case 7:
-								columnNumber++;
-								break;
-							case 8:
-								columnNumber++;
-								break;
-							case 9:
-								columnNumber++;
-								break;
-							case 10:
-								if (!header) {
-									if (t.startsWith("\"") && t.length() > 2) {
-										mappings += t;
-										if (!foundMapping) {
-											columnNumber = 1;
-											mappings = "";
-										}
+
+							}
+							columnNumber++;
+							break;
+						case 4:
+							if (errorString.length() < 1) {
+
+								if (cs.getConcept(t) != null) {
+									Concept concept = cs.getConcept(t);
+									if (concept.getConceptMappings() == null) {
+										List<ConceptMap> conceptMappingsList = new ArrayList<ConceptMap>();
+										conceptMappingsList.add(conceptMap);
+										concept.setConceptMappings(conceptMappingsList);
 									} else {
+										conceptMappings = concept
+												.getConceptMappings();
+										conceptMappings.add(conceptMap);
+										concept.setConceptMappings(conceptMappings);
+
+									}
+									try {
+										cs.saveConcept(concept);
+									} catch (APIException e) {
+										errorString = " " + errorString + "\""
+												+ e + "\"";
+										e.printStackTrace();
+									}
+
+								}
+							}
+							columnNumber++;
+							break;
+						case 5:
+							columnNumber++;
+							break;
+						case 6:
+							columnNumber++;
+							break;
+						case 7:
+							columnNumber++;
+							break;
+						case 8:
+							columnNumber++;
+							break;
+						case 9:
+							columnNumber++;
+							break;
+						case 10:
+							if (!header) {
+								if (t.startsWith("\"") && t.length() > 2) {
+									mappings += t;
+									if (!foundMapping) {
 										columnNumber = 1;
 										mappings = "";
 									}
+								} else {
+									columnNumber = 1;
+									mappings = "";
 								}
-								
-								break;
-							default:
-								columnNumber++;
-								log.error(ui.message("conceptmanagementapps.file.line.error") + columnNumber);
-								break;
-						
+							}
+
+							break;
+						default:
+							columnNumber++;
+							log.error(ui
+									.message("conceptmanagementapps.file.line.error")
+									+ columnNumber);
+							break;
+
 						}
 					}
 				}
@@ -194,37 +217,37 @@ public class UploadSpreadsheetPageController {
 					linesWithErrors.add(errorString + "," + line + "\n");
 				}
 			}
-			
-		}
-		catch (FileNotFoundException e) {
+
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		catch (IOException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			IOUtils.closeQuietly(br);
-			
+
 		}
 		return writeToFile(linesWithErrors, sourceId);
-		
+
 	}
-	
-	public void get(UiSessionContext sessionContext, PageModel model) throws Exception {
-		
+
+	public void get(UiSessionContext sessionContext, PageModel model)
+			throws Exception {
+
 	}
-	
+
 	private FileDownload writeToFile(ArrayList<String> lines, String sourceId) {
 		String line = "";
 		for (String aline : lines) {
 			line += aline;
 		}
 		String s = new SimpleDateFormat("dMy_Hm").format(new Date());
-		
+
 		String contentType = "text/csv;charset=UTF-8";
-		String errorFilename = "conceptsMissingMappingsErrors" + s + "_" + sourceId + ".csv";
+		String errorFilename = "conceptsMissingMappingsErrors" + s + "_"
+				+ sourceId + ".csv";
 		return new FileDownload(errorFilename, contentType, line.getBytes());
 	}
-	
+
 }
